8cf4be47c4fa9b244c46f9ceb41a5cb9ef53f2d3
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-06-04 12:48:37 -0600

    fixed resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
         <nav className="social">
         <a href="https://www.facebook.com/landonfolkers" className="fa fa-facebook"><span className="_hidden"></span></a>
         <a href="https://twitter.com/LandonFolkers" className="fa fa-twitter"><span className="_hidden"></span></a>
-        <a href="https://www.instagram.com/thelegendoflandon/?hl=en" className="fa fa-instagram"><span className="_hidden"></span></a>
+        <a href="https://www.instagram.com/landonfolkers/?hl=en" className="fa fa-instagram"><span className="_hidden"></span></a>
     </nav>
     </footer>
 }
